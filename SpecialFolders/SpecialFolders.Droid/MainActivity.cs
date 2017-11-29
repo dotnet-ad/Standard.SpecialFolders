f104ebe52e2108bf9fc0e9d4b82d6e3314ba205e
Aloïs Deniel <alois.deniel@gmail.com>
2017-11-29 11:51:27 +0100

    Initial platforms.

---
+++ SpecialFolders/SpecialFolders.Droid/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace SpecialFolders.Droid
+{
+    [Activity(Label = "SpecialFolders.Droid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Helper.LogSpecialFolders();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ SpecialFolders/SpecialFolders/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SpecialFolders
+{
+    public static class Helper
+    {
+        public static void LogSpecialFolders()
+        {
+            foreach (var value in Enum.GetValues(typeof(Environment.SpecialFolder)))
+            {
+                var folder = (Environment.SpecialFolder)value;
+                var path = Environment.GetFolderPath(folder);
+                Debug.WriteLine($"[{folder}] : {path}");
+            }
+        }
+    }
+}
